854b7cd2de4b1cf8658e5b5fd76ee5882a22abc5
princessbeverly <01alaan.princessbeverly@gmail.com>
2026-04-05 20:31:40 +0800

    updating loading screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 
 void main() {
@@ -24,17 +25,61 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFE6D9D9),
-      body: const Center(
-        child: Text(
-          "bev's reader",
-          style: TextStyle(
-              fontSize: 32.44,
-              fontWeight: FontWeight.w700,
-              fontFamily: 'Poppins',
-            color: const Color(0xFF9EAD68),
+      backgroundColor: const Color(0xFFFBC0A6),
+      body: Stack(
+        children: [
+
+          Positioned(
+            top: 352,
+            left: 57,
+            child: _BlurredCircle(color: Colors.white.withOpacity(0.6), radius: 70),
           ),
-        ),
+          Positioned(
+            top: 292,
+            left: 211,
+            child: _BlurredCircle(color: Colors.white.withOpacity(0.6), radius: 90),
+          ),
+          Positioned(
+            top: 507,
+            left: 190,
+            child: _BlurredCircle(color: Colors.white.withOpacity(0.6), radius: 69),
+          ),
+          Center(
+            child: Text(
+              "bev's reader",
+              style: TextStyle(
+                fontSize: 32.44,
+                fontWeight: FontWeight.w700,
+                fontFamily: 'Poppins',
+                color: const Color(0xFF9EAD68),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _BlurredCircle extends StatelessWidget {
+  final Color color;
+  final double radius;
+
+  const _BlurredCircle({required this.color, required this.radius});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: radius * 2,
+      height: radius * 2,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: color,
+      ),
+      child: BackdropFilter(
+        // Adjust SigmaX/Y to increase or decrease the blur intensity
+        filter: ui.ImageFilter.blur(sigmaX: 60.0, sigmaY: 60.0),
+        child: Container(color: Colors.transparent),
       ),
     );
   }
