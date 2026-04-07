566eee838e9899dd0df18c0a26679c049e6bcdb1
princessbeverly <01alaan.princessbeverly@gmail.com>
2026-04-07 08:41:21 +0800

    Implemented home_page | loading_page | modified main page

---
+++ lib/home_page.dart
@@ -0,0 +1,18 @@
+import 'dart:ui' as ui;
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+    const HomePage({super.key});
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            backgroundColor: const Color(0xFFFBC0A6),
+            body: Stack(
+                children: [
+                    
+                ],
+            ),
+        ),
+    }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
+import 'loading_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,75 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: HomePage(),
+      home: LoadingPage(),
       debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-// Your main screen
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFFFBC0A6),
-      body: Stack(
-        children: [
-
-          Positioned(
-            top: 352,
-            left: 57,
-            child: _BlurredCircle(color: Colors.white.withOpacity(0.6), radius: 70),
-          ),
-          Positioned(
-            top: 292,
-            left: 211,
-            child: _BlurredCircle(color: Colors.white.withOpacity(0.6), radius: 90),
-          ),
-          Positioned(
-            top: 507,
-            left: 190,
-            child: _BlurredCircle(color: Colors.white.withOpacity(0.6), radius: 69),
-          ),
-          Center(
-            child: Text(
-              "bev's reader",
-              style: TextStyle(
-                fontSize: 32.44,
-                fontWeight: FontWeight.w700,
-                fontFamily: 'Poppins',
-                color: const Color(0xFF9EAD68),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class _BlurredCircle extends StatelessWidget {
-  final Color color;
-  final double radius;
-
-  const _BlurredCircle({required this.color, required this.radius});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: radius * 2,
-      height: radius * 2,
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        color: color,
-      ),
-      child: BackdropFilter(
-        // Adjust SigmaX/Y to increase or decrease the blur intensity
-        filter: ui.ImageFilter.blur(sigmaX: 60.0, sigmaY: 60.0),
-        child: Container(color: Colors.transparent),
-      ),
     );
   }
 }
